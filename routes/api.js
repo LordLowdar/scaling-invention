c44e7ff4173ad31b7fefc46da05fc917a5e163a0
Julian Williams <julian.th.williams@gmail.com>
2021-10-31 13:48:48 -0700

    Got the create workout route up, but im not sure the PUT route for adding exercises in isnt working

--- routes/api.js
+++ routes/api.js
@@ -10,4 +10,21 @@ router.get('/workouts', async (req, res) => {
   }
 });
 
+router.post('/workouts', async (req, res) => {
+  try {
+    const newWorkout = await workout.create();
+    res.status(200).send(newWorkout);
+  } catch (err) {
+    res.status(500);
+  }
+});
+
+router.put('/workouts/:id', async (req, res) => {
+  try {
+    const addExercise = await workout.createIndexes(excercises);
+    res.status(200).send(addExercise);
+  } catch (err) {
+    res.status(500);
+  }
+});
 module.exports = router;
